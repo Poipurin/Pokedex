8d31525e1aaa379eb7185fe8baa3791a06558ffb
Poipurin <b.pinorojo@gmail.com>
2023-02-11 20:30:39 -0300

    imágenes animadas versión blanco y negro

--- assets/js/poke.js
+++ assets/js/poke.js
@@ -2,9 +2,9 @@ const btnSubmit = document.getElementById("btnSubmit");
 btnSubmit.addEventListener('click', obtenerPokemon,);
 const pokeInput = document.getElementById("buscador");
 const pokemonImg = document.getElementById("pokeImg");
-const pokemonCard = document.getElementById("card");
+const pokemonCard = document.getElementById("pokeCard");
 pokemonCard.classList.add("invisible");
-const pokError = document.getElementById("card-error");
+const pokError = document.getElementById("pokError");
 pokError.classList.add("invisible");
 
 async function obtenerPokemon() {
@@ -18,22 +18,30 @@ async function obtenerPokemon() {
     if (poke.ok) {
         let pokeJson = await poke.json();
         mostrarPokemon(pokeJson); 
-    }
-    else {
-        pokError();
-    }
+    } return;
 }
 
+/* function pokError(){
+    
+} */
+
 function siEs(pokeInputValue) {
-    if (pokeInputValue.length >= 1 && pokeInputValue.length < 20) return true;
+    if (pokeInputValue.length >= 1 && pokeInputValue.length < 13) return true;
     else {
         return false;
     }
 }
 
+function pokeValue(si) {
+    if (si) pokeInput.style.border = '';
+    else {
+        pokeInput.style.border = '';
+    }
+}
+
 function mostrarPokemon(pokemon) {
     pokemonCard.classList.remove("invisible");
-    pokemonImg.setAttribute('src', pokemon.sprites.front_default);
+    pokemonImg.setAttribute('src', pokemon.sprites.versions['generation-v']['black-white'].animated.front_default);
     document.getElementById("nombre").textContent = pokemon.name;
     document.getElementById("id").textContent = pokemon.id;
     let altura = pokemon.height / 10;
@@ -46,14 +54,14 @@ function mostrarPokemon(pokemon) {
         return poke.type.name;
     });
     let tipoStr = nombreTipo.toString();
-    document.getElementById('tipo').textContent = tipoStr.replace(',' , ' ');
+    document.getElementById("tipo").textContent = tipoStr.replace(',' , ' ');
 
     let habilidades = pokemon.abilities;
     let nombreHab = habilidades.map(function(poke) {
         return poke.ability.name;
     });
     let habStr = nombreHab.toString();
-    document.getElementById('habilidades').textContent = habStr;
+    document.getElementById("habilidades").textContent = habStr;
 
 }
 
